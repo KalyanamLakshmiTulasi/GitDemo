9a96e8d2bea3d305c743474b32679f25758cd258
tulasi <lakshmitulasi79@gmail.com>
2025-11-29 21:08:36 +0530

    Owner commited change2

--- src/javaPracticeDataTypes/DatatypeSizes.java
+++ src/javaPracticeDataTypes/DatatypeSizes.java
@@ -5,6 +5,8 @@ public class DatatypeSizes {
 	public static void main(String[] args) {
 		
 		System.out.println("The guy who cloned updated the project");
+		System.out.println("Original owner also updated the project");
+		
 		
 	}
 
