4cc113da079cfbe31adcaa93edf2e84dbe187ff6
tulasi <lakshmitulasi79@gmail.com>
2025-11-29 20:45:34 +0530

    Cloned guy committed change1

--- src/javaPracticeDataTypes/DatatypeSizes.java
+++ src/javaPracticeDataTypes/DatatypeSizes.java
@@ -1,5 +1,11 @@
 package javaPracticeDataTypes;
 
 public class DatatypeSizes {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("The guy who cloned updated the project");
+		
+	}
 
 }
